886c9ad4d4d768bf03a363bab1b7430fb0e0c0f9
jonathanssm <jonathan.ssm@hotmail.com>
2025-09-06 08:31:35 -0300

    :recycle: refactor - ajuste na camada de teste

--- src/test/java/com/jonathanssm/portfoliobackend/PortfolioBackendApplicationTests.java
+++ src/test/java/com/jonathanssm/portfoliobackend/PortfolioBackendApplicationTests.java
@@ -2,12 +2,17 @@ package com.jonathanssm.portfoliobackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@EmbeddedKafka(partitions = 1, controlledShutdown = true)
 class PortfolioBackendApplicationTests {
 
 	@Test
 	void contextLoads() {
+        // teste
 	}
 
 }
