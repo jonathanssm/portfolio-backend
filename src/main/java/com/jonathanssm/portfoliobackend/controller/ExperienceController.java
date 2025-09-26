2cd4e34b0c2cf16dff8d70c03d5102a5caa92afb
Jonathan Sebastian da Silva Moraes <33239505+jonathanssm@users.noreply.github.com>
2025-09-26 15:31:25 -0300

    ✨ Implement global /api prefix for all REST controllers
    - Create WebConfig to automatically add /api prefix to all @RestController classes
    - Revert ExperienceController to use /experiences mapping
    - More elegant and maintainable solution for API routing
    - Future controllers will automatically get /api prefix

--- src/main/java/com/jonathanssm/portfoliobackend/controller/ExperienceController.java
+++ src/main/java/com/jonathanssm/portfoliobackend/controller/ExperienceController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @Validated
 @RestController
-@RequestMapping("/api/experiences")
+@RequestMapping("/experiences")
 @RequiredArgsConstructor
 public class ExperienceController {
 
